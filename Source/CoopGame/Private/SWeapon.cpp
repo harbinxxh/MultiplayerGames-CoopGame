dcb2ae6def11705882a1c5e69ec5cd756d5b5f8b
harbinxxh <harbinxxh@gmail.com>
2018-09-14 23:11:24 +0800

    08 Coop Game 3_ Weapon Extras : 065 Create Debug Console Variables(FAutoConsoleVariableRef)

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -7,6 +7,12 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef CVARDebugWeaponDrawing(
+	TEXT("COOP.DebugWeapons"),
+	DebugWeaponDrawing,
+	TEXT("Draw Debug Lines for Weapons"),
+	ECVF_Cheat);
 
 // Sets default values
 ASWeapon::ASWeapon()
@@ -88,7 +94,10 @@ void ASWeapon::Fire()
 			TraceEndPoint = Hit.ImpactPoint;
 		}
 
-		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		if (DebugWeaponDrawing > 0)
+		{
+			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		}
 
 		// 枪口火花特效
 		if (MuzzleEffect)
