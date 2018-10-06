e031a6afccac35fe009fc5164985c2a13530ed75
harbinxxh <harbinxxh@gmail.com>
2018-10-06 20:57:44 +0800

    11 Coop Game 6_ Basic AI : 092 Add Move-to Logic(UNavigationSystemV1::FindPathToActorSynchronously()、UAIBlueprintHelperLibrary::SimpleMoveToLocation() or SimpleMoveToActor())

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -1,7 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "STrackerBot.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+//#include "AI/NavigationSystemBase.h"
+#include "GameFramework/Character.h"
+#include "NavigationSystem/Public/NavigationSystem.h"
+#include "NavigationSystem/Public/NavigationPath.h"
 
 // Sets default values
 ASTrackerBot::ASTrackerBot()
@@ -19,6 +24,23 @@ void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+FVector ASTrackerBot::GetNextPathPoint()
+{
+	// Hack, to get player location
+	ACharacter* PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
+
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+
+	if (NavPath->PathPoints.Num() > 1)
+	{
+		// Return next point in zhe path
+		return NavPath->PathPoints[1];
+	}
+
+	// Failed to find path
+	return GetActorLocation();
 }
 
 // Called every frame
--- Source/CoopGame/Public/AI/STrackerBot.h
+++ Source/CoopGame/Public/AI/STrackerBot.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -21,6 +21,8 @@ protected:
 
 	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
 	UStaticMeshComponent* MeshComp;
+
+	FVector GetNextPathPoint();
 
 public:	
 	// Called every frame
