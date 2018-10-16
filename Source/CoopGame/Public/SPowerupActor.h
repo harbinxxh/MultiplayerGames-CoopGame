86106c6edb2043bfec96c694c29346954c909b93
harbinxxh <harbinxxh@gmail.com>
2018-10-16 18:15:58 +0800

    12 Coop Game 7_ Power-ups : 105 Create the Power-up Class(添加SPowerupActor类)

---
+++ Source/CoopGame/Public/SPowerupActor.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SPowerupActor.generated.h"
+
+UCLASS()
+class COOPGAME_API ASPowerupActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASPowerupActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/* Time between powerup ticks */
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	float PowerupInterval;
+
+	/* Total times we apply the powerup effect */
+	UPROPERTY(EditDefaultsOnly, Category = "Powerups")
+	float TotalNrOfTicks;
+
+	FTimerHandle TimerHandle_PowerupTick;
+
+	// Total number of ticks applied
+	int32 TicksProcessed;
+
+	UFUNCTION()
+	void OnTickPowerup();
+
+public:
+
+	// 这个函数将会在我们 Pickup 的时候被调用而不是在生成的时候
+	void ActivatePowerup();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnActivated();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnPowerupTicked();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Powerups")
+	void OnExpired();
+	
+};
