e79e68f0cbd998383c640b6ab51a6a401f17892f
harbinxxh <harbinxxh@gmail.com>
2018-12-01 23:45:49 +0800

    13 Coop Game 8_ GameMode Setup : 120 Add Wave Based Spawning

--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,7 +14,40 @@ class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	FTimerHandle TimerHandle_NextWaveStart;
+
+	// Bots to spawn in current wave
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+protected:
+
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
 	
-	
-	
+	// Start Spawning Bots
+	void StartWave();
+
+	// Stop Spawning Bots
+	void EndWave();
+
+	// Set timer for next startwave
+	void PrepareForNextWave();
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
 };
